cc441c0af1f4334cced03e1ae36125b8a499aa0b
Igor Baiborodine <igor@kiroule.com>
2023-12-16 17:36:15 -0500

    feat(primitivetypes): add solution for compute power (#8)

--- primitivetypes/computedivide.go
+++ primitivetypes/computedivide.go
@@ -1,7 +1,7 @@
 package primitivetypes
 
 // ComputeDivide returns integer that is result of dividing two non-negative integers.
-// The total time complexity of addition is 0(n), where n is the width of the operands.
+// The time complexity of addition is O(n).
 func ComputeDivide(x, y uint64) uint64 {
 	var quo uint64
 	pow := 32
--- primitivetypes/computemultiply.go
+++ primitivetypes/computemultiply.go
@@ -3,7 +3,7 @@ package primitivetypes
 import "math/bits"
 
 // ComputeMultiply returns integer that is result of multiplying two non-negative integers.
-// The total time complexity of addition is 0(n), where n is the width of the operands.
+// The total time complexity of addition is O(n), where n is the width of the operands.
 func ComputeMultiply(x, y uint64) uint64 {
 	var sum uint64
 
